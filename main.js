0126b495531ae69258c7490f7e9f6a3da3f378a8
Ratulxhasan1 <rh060166@gmail.com>
2025-05-16 02:52:15 +0600

    Update main.js

--- main.js
+++ main.js
@@ -12,13 +12,13 @@ console.log(AllResult);
 // Even or Odd checker 
 
 function isEven(num) {
-    // if(num % 2 == 0){
+    // if(num % 2 === 0){
     //     return "Even"
     // } else {
     //     return "Odd"
     // }
 
-    return num % 2 == 0 ? "Even" : "Odd"
+    return num % 2 === 0 ? "Even" : "Odd"
 }
 
 console.log(isEven(6));
@@ -46,11 +46,52 @@ console.log(FirstCharacter("Hello"));
 
 
 // Number is positive or Nagetive 
+
 function CheckNum(num) {
-    if(num>=0){
-        return "Positive"
-    } else {
-        return "Negative"
+    // if(num>=0){
+    //     return "Positive"
+    // } else {
+    //     return "Negative"
+    // }
+
+    return num >= 0 ? "Positive" : "negative";
+
+   
+}
+console.log(CheckNum(-9));
+
+// Multiply Two Number
+function multiply(x,y){
+    if(typeof x !== "number"){
+        return "1st number is not a number"
+    } else if(typeof y !== "number"){
+        "2nd number is not a number"
+    }else{
+        return x * y;
     }
 }
-console.log(CheckNum(4));
+console.log(multiply(10,30));
+
+
+// Convert to minute to  second 
+
+function minuteTosecond(min) {
+    return min * 60
+}
+console.log(minuteTosecond(60));
+
+// Concatenate Two Strings
+
+function concatenate(x,y){
+    return x + " " + y
+} 
+   console.log(concatenate("Hello" , "Javascript"));
+
+//   Check if a strings contains a word
+
+function checkSTR(sentence, word){
+        return sentence.includes(word)
+}
+    console.log(checkSTR("I love Javascript" , "Javascript"));
+    
+
